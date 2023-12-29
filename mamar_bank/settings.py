c62793c1fdef65e752d9323e62b0436371659176
Tipu Sultan <sultanmdtipu1996@gmail.com>
2023-12-29 19:13:39 +0800

    changes-4

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -43,10 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'accounts',
     'core',
     'transaction',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
